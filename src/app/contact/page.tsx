3d91e84abf76ae3e5697bf7690bf58d62c59d5bc
Mael <maelroulette12@gmail.com>
2026-01-31 17:45:50 +0100

    commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage () {
+  return (
+    <main>
+      <h1 className="text-6xl lg:text-8xl font-bold mb-8 px-5 lg:px-12 pt-8 font-jubble text-secondary">Contact</h1>
+    </main>
+  );
+}
