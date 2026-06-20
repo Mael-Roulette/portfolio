1e3a129222af79d9780da2de5bd16948cdf70234
Mael Roulette <maelroulette12@gmail.com>
2026-06-20 11:13:59 +0200

    update seo hero

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -34,7 +34,6 @@ export default function HeroSection () {
         "-=0.2"
       );
 
-      // Animation du texte
       const split = new SplitText( ".split", {
         type: "lines, words",
         linesClass: "line",
@@ -59,7 +58,7 @@ export default function HeroSection () {
       className="h-[calc(100dvh-54px)] lg:h-[calc(100dvh-69px)] grid grid-rows-3 md:grid-rows-[2fr_1fr_2fr]"
     >
       <div className="h-full border-b border-b-foreground/30">
-        <div
+        <h1
           className="px-5 w-fit lg:w-4/5 2xl:w-3/4 border-r border-r-foreground/30 h-full flex flex-col justify-center"
           aria-label="Roulette Maël"
         >
@@ -75,7 +74,7 @@ export default function HeroSection () {
           >
             Maël
           </p>
-        </div>
+        </h1>
       </div>
 
       <div className="hero-btn flex flex-col md:flex-row gap-5 items-center justify-center">
@@ -89,7 +88,7 @@ export default function HeroSection () {
 
       <div className="flex flex-col items-end border-y border-y-foreground/30">
         <div className="px-5 w-fit lg:w-4/5 2xl:w-3/4 border-l border-l-foreground/30 h-full flex flex-col justify-center">
-          <h1
+          <h2
             className="split text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-right uppercase font-bold"
             aria-label="Développeur Web & Mobile"
           >
@@ -98,7 +97,7 @@ export default function HeroSection () {
               <span className="font-jubble text-secondary">Web</span> &{" "}
               <span className="font-jubble text-secondary">Mobile</span>
             </span>
-          </h1>
+          </h2>
         </div>
       </div>
     </section>
